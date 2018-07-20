711682800a7b14aef09009ed89f864b9e5fedac9
Chandan Kushtawar <chandan.kushtawar@icloud.com>
2018-07-20 16:36:13 +0530

    changed cors

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var productController = require('./controllers/productController.js');
 
 var app= express();
 app.use(bodyParser.json());
-app.use(cors({ origin: 'http://13.127.110.85:4200'}));
+app.use(cors({ origin: 'http://13.127.110.85'}));
 app.listen(3000, () => console.log('API is running on localhost:3000'));
 
 app.use('/products',productController);
