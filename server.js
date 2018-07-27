a41b8b9053f4e21dd7a1e48d1a4df0004c01018b
Chandan Kushtawar <chandan.kushtawar@icloud.com>
2018-07-27 23:27:41 +0530

    Working with parameters experess part

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // Import dependencies
-
+'use strict';
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -9,7 +9,22 @@ var productController = require('./controllers/productController.js');
 
 var app= express();
 app.use(bodyParser.json());
-app.use(cors({ origin: 'http://13.127.110.85'}));
+
+const fs = require('fs');
+
+let rawdata = fs.readFileSync('env.json');  
+let envdetails = JSON.parse(rawdata);  
+let myenv= envdetails.env;
+let myinstanceurl=envdetails.instanceurl;
+console.log(myenv);
+console.log(myinstanceurl); 
+
+
+app.use(cors({ origin: myinstanceurl }));
+
+
+
+//app.use(cors({ origin: 'http://localhost:4200' }));
 app.listen(3000, () => console.log('API is running on localhost:3000'));
 
 app.use('/products',productController);
